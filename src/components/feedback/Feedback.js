e7cee64f972512908354e7e4fadb26a82e4dde55
ING-JORGE2022 <116320081+ING-JORGE2022@users.noreply.github.com>
2023-07-10 23:59:30 -0500

    solucion

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -5,7 +5,7 @@ const FeedbackApp =() =>{
    const [good , setGood] = useState (0)
    const [neutral , setNeutral] = useState (0)
    const [bad , setBad] = useState (0)
-   const [reset, setReset] = useState (false)
+   
 ////////////////////////////////////////////
     const AumentarContadorGood = () => {
         setGood(good + 1)
@@ -32,7 +32,7 @@ const FeedbackApp =() =>{
         setGood(0);
         setBad(0);
         setNeutral(0);
-        setReset(true);
+        
     }
      const Notification = (props) =>{
         return <h2>{props.mensaje}</h2>
