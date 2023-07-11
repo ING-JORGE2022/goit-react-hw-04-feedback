7c97dde661324188fd6a65c71d547a3f05b95458
ING-JORGE2022 <116320081+ING-JORGE2022@users.noreply.github.com>
2023-07-10 23:15:41 -0500

    modulo 4.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import  Feedback from '../components/feedback/Feedback';
+
+function App (){
+  return(
+    <>
+    <Feedback/>
+    </>
+  )
+}
+export default App;
---
+++ src/components/feedback/Feedback.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import style from './feebback.module.css';
+/// se uso HOOK DE useState
+const FeedbackApp =() =>{
+   const [good , setGood] = useState (0)
+   const [neutral , setNeutral] = useState (0)
+   const [bad , setBad] = useState (0)
+   const [reset, setReset] = useState (false)
+////////////////////////////////////////////
+    const AumentarContadorGood = () => {
+        setGood(good + 1)
+        console.log("Good :" , good)
+    } 
+    const AumentarContadorNeutral = () => {
+        setNeutral( neutral + 1)
+        console.log("Neutro :" , neutral)
+    }
+    const AumentarContadorBad = () => {
+       setBad(bad + 1)
+        console.log("Bad :",  bad)
+    }
+    // ////////////////////////////////////
+    const countTotalFeedback = () =>{
+    
+    return good + neutral+ bad;
+    }
+    const countPositiveFeedbackPercentage = () =>{
+        return countTotalFeedback() > 0 ? Math.round((good/countTotalFeedback ())*100) : 0
+    }
+
+    const Reset = () =>{
+        setGood(0);
+        setBad(0);
+        setNeutral(0);
+        setReset(true);
+    }
+     const Notification = (props) =>{
+        return <h2>{props.mensaje}</h2>
+    }
+   
+        return (
+            <>
+            
+          <div className={style.container}>
+              <h2>Please leave Feedback</h2>
+              <br></br>
+             <div className={style.containerbutton}>
+                 <button className={style.button} onClick={AumentarContadorGood}>Good</button>
+                 <button className={style.button}  onClick={AumentarContadorNeutral}>Neutral</button>
+                 <button className={style.button} onClick={AumentarContadorBad}>Bad</button>
+             </div>
+              <br></br>
+              <h2>Statictis</h2>
+              {countTotalFeedback () > 0 ? 
+              <div>
+                  
+                  <ul>
+                      <li>Good: {good} </li>
+                      <li>Neutral: {neutral}</li>
+                      <li>Bad: {bad}</li>
+                      <li>Positive Feedback: {countPositiveFeedbackPercentage()} % </li>
+                  </ul>
+              </div>    
+            :<Notification mensaje="There is no feedback"/> }
+          <button className={style.button} onClick={Reset}>Reset</button>
+          </div>
+        </>
+            
+        );
+    }
+export default  FeedbackApp;
